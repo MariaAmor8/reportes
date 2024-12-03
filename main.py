f17dad0328a2bb6a540d0c00070dc3cd40a62799
Maria Amorocho <m.amorocho@uniandes.edu.co>
2024-12-03 16:50:06 -0500

    router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 import views
 from models import db
+import views.reportes_views
 
 
 def create_app():
@@ -15,7 +16,7 @@ def create_app():
     async def on_startup():
         await db.set_reportes_db()
 
-    app.include_router(views.router)
+    app.include_router(views.reportes_views.router)
 
     return app
 
